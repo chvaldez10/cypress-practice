1dd3aeb0906c8069510b29754cc425250acbc5d7
chvaldez10 <christianvaldez021@gmail.com>
2024-03-17 20:03:06 -0600

    update

--- green_cart/cypress/e2e/home/homepage.cy.js
+++ green_cart/cypress/e2e/home/homepage.cy.js
@@ -5,7 +5,20 @@ describe("My First Test", () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-    cy.get(".products").should("have.length", 5);
+    cy.get(".product").should("have.length", 5);
     cy.get(".product:visible").should("have.length", 4);
+    cy.get(":nth-child(3) > .product-action > button").click();
+    cy.get(".products")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product.name").text();
+        if (textVeg.includes("Cashews")) {
+          cy.wrap($el).find("button").click();
+        }
+      });
+
+    cy.get(".brand").then((logoElement) => {
+      cy.log(logoElement.text());
+    });
   });
 });
