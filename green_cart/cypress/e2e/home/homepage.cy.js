c4cb7113aba8676b669e71bbbadd1dd9362abb14
chvaldez10 <christianvaldez021@gmail.com>
2024-03-30 14:37:48 -0600

    finished green cart tutorial

--- green_cart/cypress/e2e/home/homepage.cy.js
+++ green_cart/cypress/e2e/home/homepage.cy.js
@@ -5,20 +5,16 @@ describe("My First Test", () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-    cy.get(".product").should("have.length", 5);
-    cy.get(".product:visible").should("have.length", 4);
-    cy.get(":nth-child(3) > .product-action > button").click();
-    cy.get(".products")
+
+    cy.get(".products").as("productLocator");
+    cy.get("@productLocator")
       .find(".product")
       .each(($el, index, $list) => {
-        const textVeg = $el.find("h4.product.name").text();
+        const textVeg = $el.find("h4.product-name").text();
         if (textVeg.includes("Cashews")) {
           cy.wrap($el).find("button").click();
         }
       });
-
-    cy.get(".brand").then((logoElement) => {
-      cy.log(logoElement.text());
-    });
+    cy.get(".cart-icon>img").click();
   });
 });
